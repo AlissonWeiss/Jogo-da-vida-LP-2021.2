cf7b3763cf250600e78ef2d8c4a2e984c379ec58
Alisson Weiss <alissonweiss@id.uff.br>
2022-02-07 21:27:48 -0300

    Funcionando

--- gameOfLife.hs
+++ gameOfLife.hs
@@ -1,10 +1,10 @@
--- m m v z 
--- z v m m
--- z v m m
 
-
+-- TYPES
+{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
+{-# HLINT ignore "Use foldr" #-}
 type Matrix = [[String]]
 data Result = Result { m :: Matrix, it :: Int }
+-- END TYPES
 
 -- EXTRACTOR
 getNumberOfIterations :: Result -> Int
@@ -12,46 +12,83 @@ getNumberOfIterations (Result _ it) = it
 
 getResult :: Result -> Matrix
 getResult (Result m _) = m
--- END -- EXTRACTOR
+-- END EXTRACTOR
 
 -- GAME LOGIC
-compareMatrix :: Matrix -> Matrix -> Bool
-compareMatrix m1 m2 = m1 == m2
+asd :: [String] -> [String]
+asd [] = []
+asd ("v" : "m" : "v" : xs) = "v" : "v" : "v" : asd xs
+asd (x : "v" : xs) = "m" : asd xs
+asd (x : "z" : xs) = "q" : asd xs
+asd (x : xs) = x : asd xs
+
+test :: Matrix -> Matrix
+test [] = []
+test (x : xs) = [asd x] ++ xs
+
+
+
+gamelogic :: Matrix -> Matrix
+gamelogic matrix = test matrix
 -- END GAME LOGIC
 
+-- qwerty :: [String] -> String
+-- qwerty ["v_"] = "v"
+-- qwerty x = "W"
+
+
+-- perfomActions :: Matrix -> Matrix
+-- perfomActions [] = []
+-- perfomActions (x : xs) = [qwerty x] : perfomActions xs
+-- COMPARE
+compareMatrix :: Matrix -> Matrix -> Bool
+compareMatrix m1 m2 = m1 == m2
+-- END COMPARE
+
 -- GAME LOOP
-gameloop :: Matrix -> Matrix -> Int -> Int -> Result
-gameloop inputMatrix outputMatrix begin end
+gameloop :: Int -> Int -> Matrix -> Matrix -> Result
+gameloop begin end inputMatrix outputMatrix
     | begin == end = Result outputMatrix begin
     | begin < end = do
-        if compareMatrix outputMatrix inputMatrix then
+        if compareMatrix inputMatrix outputMatrix then
             Result outputMatrix begin
         else
-            gameloop outputMatrix inputMatrix (begin + 1) end
+            gameloop (begin + 1) end inputMatrix $ gamelogic inputMatrix 
     | otherwise = Result outputMatrix begin
 
 -- END GAME LOOP
 
 -- PRINT MATRIX
-matrix2strHelper :: String -> String
-matrix2strHelper x = x
-
 matrix2str :: [String] -> String
 matrix2str [] = ""
-matrix2str (x : xs) = matrix2strHelper x ++ "|" ++ matrix2str xs
+matrix2str (x : xs) = x ++ "|" ++ matrix2str xs
 -- END PRINT MATRIX
 
+
+-- GAME INSTANCE
+-- m m v z 
+-- z v m m
+-- z v m m
 main :: IO ()
 main = do    
-    let matrix = [["m", "m", "v", "z"],["z", "v", "m", "m"],["z", "v", "m", "m"]]
-    -- let outputMatrix = Matrix ['a', 'a']
+    let matrix = [["v", "m", "v", "z"],["z", "v", "m", "m"],["z", "v", "m", "m"]]
+
     let n = 5
 
-    let finalMatrix = gameloop matrix matrix 1 n
+    let original = map matrix2str matrix
+    putStr "\nMatriz original:\n"
+    mapM_ putStrLn original
+
+    -- print diagonal
+    -- let x = zipWith (!!) matrix [0..]
+    -- mapM_ putStr x
+
+    let finalMatrix = gameloop 1 n matrix [[]]
 
     putStr "Numero de iteracoes: "
     print $ getNumberOfIterations finalMatrix
 
     putStr "\nResultado do jogo:\n"
+    
     let body = map matrix2str $ getResult finalMatrix
     mapM_ putStrLn body
