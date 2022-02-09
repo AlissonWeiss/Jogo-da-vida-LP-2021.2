348e1df1cdd354e932fa4df1a362c0d1e1ad5d13
Alisson Weiss <alissonweiss@id.uff.br>
2022-02-08 21:02:12 -0300

    Alterando para leitura de arquivo

--- gameOfLife.hs
+++ gameOfLife.hs
@@ -1,9 +1,5 @@
-
 -- TYPES
-{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
-{-# HLINT ignore "Use foldr" #-}
 type Matrix = [String]
-data Matrix2 = Matrix2 { content :: Matrix, contentData :: Matrix };
 data Result = Result { m :: Matrix, it :: Int }
 -- END TYPES
 
@@ -15,6 +11,33 @@ getResult :: Result -> Matrix
 getResult (Result m _) = m
 -- END EXTRACTOR
 
+-- CONVERTER
+
+strToInt :: String -> Int
+strToInt = read
+
+intToStr :: Int -> String
+intToStr = show
+
+-- END CONVERTER
+
+-- FILE FUNCTIONS
+
+readInputFile :: FilePath -> IO(Int, Int, Int, Matrix)
+readInputFile file = fmap(readLine . words) (readFile file)
+
+--readLine :: [String] -> (Int, Int, Int, [String])
+readLine :: [String] -> (Int, Int, Int, Matrix)
+readLine (iteracao : linhas : colunas : matriz) =
+    (i, l, c, m)
+    where
+        i = strToInt iteracao
+        l = strToInt linhas
+        c = strToInt colunas
+        m = matriz
+
+-- END FILE FUNCTIONS
+
 -- GAME LOGIC
 
 gamelogic :: Matrix -> Matrix
@@ -24,14 +47,6 @@ gamelogic x = x
 
 -- END GAME LOGIC
 
--- qwerty :: [String] -> String
--- qwerty ["v_"] = "v"
--- qwerty x = "W"
-
-
--- perfomActions :: Matrix -> Matrix
--- perfomActions [] = []
--- perfomActions (x : xs) = [qwerty x] : perfomActions xs
 -- COMPARE
 compareMatrix :: Matrix -> Matrix -> Bool
 compareMatrix m1 m2 = m1 == m2
@@ -45,7 +60,7 @@ gameloop begin end inputMatrix outputMatrix
         if compareMatrix inputMatrix outputMatrix then
             Result outputMatrix begin
         else
-            gameloop (begin + 1) end inputMatrix $ gamelogic inputMatrix 
+            gameloop (begin + 1) end inputMatrix $ gamelogic inputMatrix
     | otherwise = Result outputMatrix begin
 
 -- END GAME LOOP
@@ -56,30 +71,31 @@ matrix2str [] _ _ = ""
 matrix2str (x : xs) index col = do
     if index == col then
         x ++ "|\n" ++ matrix2str xs 1 col
-    else    
+    else
         x ++ "|" ++ matrix2str xs (index + 1) col
 -- END PRINT MATRIX
 
 -- GAME INSTANCE
--- m v z z
--- v v m m
--- z v m m
 main :: IO ()
-main = do    
-    let matrix = ["m", "v", "z", "z", "v", "v", "m", "m", "z", "v", "m", "m"]
+main = do
 
-    let n = 5
+    -- Inicio teste 1
+    putStrLn "\nTeste 1.\n"
+    (iteracao_t1, linhas_t1, colunas_t1, matriz_t1) <- readInputFile "teste_1.txt" -- Lê os dados do arquivo 
+    putStrLn ("Iterações: " ++ intToStr iteracao_t1)
+    putStrLn ("Linhas: " ++ intToStr linhas_t1)
+    putStrLn ("Colunas: " ++ intToStr colunas_t1)
+    let matriz_t1_str = matrix2str matriz_t1 1 colunas_t1
+    putStrLn ("\nMatriz inicial\n" ++ matriz_t1_str)
 
-    let original = matrix2str matrix 1 4
-    putStr "\nJogo Inicial:\n"
-    putStr original
-
-    let finalMatrix = gameloop 1 n matrix []
-
-    putStr "\nNumero de iteracoes: "
-    print $ getNumberOfIterations finalMatrix
+    let gameResult_t1 = gameloop 1 iteracao_t1 matriz_t1 []
+    let iteracoes_gastas_t1 = getNumberOfIterations gameResult_t1
+    putStrLn ("Número de iterações gastas: " ++ intToStr iteracoes_gastas_t1)
 
     putStr "\nResultado do jogo:\n"
-    
-    let final = matrix2str (getResult finalMatrix) 1 4
-    putStr final
+
+    let final_matriz_t1 = matrix2str (getResult gameResult_t1) 1 colunas_t1
+    putStr final_matriz_t1
+
+    putStrLn "\nFim Teste 1.\n"
+    -- Fim teste 1
